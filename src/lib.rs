9a688704718fd8abbbc23eaa7ce7124554d4aa0f
Aatif Syed <aatif.syed@metaswitch.com>
2021-06-08 05:10:45 +0100

    destructure request

--- src/lib.rs
+++ src/lib.rs
@@ -4,39 +4,40 @@ use std::fmt::Debug;
 
 pub use mpsc::Receiver;
 pub type Response<T> = oneshot::Receiver<T>;
-#[must_use = "You must respond to the request"] // TODO doesn't work
-pub struct Request<Req, Resp> {
-    pub request: Req,
-    responder: oneshot::Sender<Resp>,
-}
+#[must_use = "You must respond to the request"]
+pub type Responder<T> = oneshot::Sender<T>;
 
-impl<Req, Resp> Request<Req, Resp>
+pub struct IncomingRequest<Req, Resp>(pub Req, pub Responder<Resp>);
+
+impl<Req, Resp> IncomingRequest<Req, Resp>
 where
     Resp: Debug,
 {
     pub fn respond(self, response: Resp) -> Result<(), Resp> {
-        self.responder.send(response)
+        self.1.send(response)
     }
 }
 
 pub struct Sender<Req, Resp> {
-    outgoing: mpsc::Sender<Request<Req, Resp>>,
+    outgoing: mpsc::Sender<IncomingRequest<Req, Resp>>,
 }
 
 impl<Req, Resp> Sender<Req, Resp> {
     pub async fn send(
         &self,
         request: Req,
-    ) -> Result<Response<Resp>, mpsc::SendError<Request<Req, Resp>>> {
+    ) -> Result<Response<Resp>, mpsc::SendError<IncomingRequest<Req, Resp>>> {
         let (responder, response) = oneshot::channel();
-        self.outgoing.send(Request { request, responder }).await?;
+        self.outgoing
+            .send(IncomingRequest(request, responder))
+            .await?;
         Ok(response)
     }
 }
 
 /// ```
 /// # async_std::task::block_on( async {
-/// let (sender, receiver) = aactor::bounded::<String, usize>(10);
+/// let (sender, receiver) = bidirectional_channel::bounded::<String, usize>(10);
 /// let response = sender.send(String::from("hello")).await.unwrap();
 ///
 /// // This side of the channel receives strings, and responds with their length
@@ -49,7 +50,10 @@ impl<Req, Resp> Sender<Req, Resp> {
 /// ```
 pub fn bounded<Req, Resp>(
     capacity: usize,
-) -> (Sender<Req, Resp>, mpsc::Receiver<Request<Req, Resp>>) {
+) -> (
+    Sender<Req, Resp>,
+    mpsc::Receiver<IncomingRequest<Req, Resp>>,
+) {
     let (sender, receiver) = mpsc::bounded(capacity);
     (Sender { outgoing: sender }, receiver)
 }
@@ -59,14 +63,14 @@ pub fn bounded<Req, Resp>(
 
 use std::ops::{Deref, DerefMut};
 
-impl<Req, Resp> Deref for Request<Req, Resp> {
+impl<Req, Resp> Deref for IncomingRequest<Req, Resp> {
     type Target = Req;
     fn deref(&self) -> &Self::Target {
-        &self.request
+        &self.0
     }
 }
-impl<Req, Resp> DerefMut for Request<Req, Resp> {
+impl<Req, Resp> DerefMut for IncomingRequest<Req, Resp> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.request
+        &mut self.0
     }
 }
