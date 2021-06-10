b22dd23bf347afe5be3fd7555db9ab56f3147df1
Aatif Syed <aatif.syed@metaswitch.com>
2021-06-10 01:14:09 +0100

    return a tuple instead

--- src/lib.rs
+++ src/lib.rs
@@ -4,68 +4,54 @@
 #[deny(missing_docs)]
 use async_std::channel;
 pub use async_std::channel::Receiver as Responder;
-use derive_more::{AsMut, AsRef, Deref, DerefMut};
 use futures::channel::oneshot;
 use std::fmt::Debug;
 pub type FutureResponse<T> = oneshot::Receiver<T>;
 
+/// Received by the [`Responder`] - represents that the [`Requester`] associated with this channel is still waiting for a response.
+/// Must be used.
 #[must_use = "You must respond to the request"]
-#[derive(AsRef, AsMut, Deref, DerefMut)]
-pub struct IncomingRequest<Req, Resp> {
-    #[as_ref]
-    #[as_mut]
-    #[deref]
-    #[deref_mut]
-    request: Req,
+pub struct UnRespondedRequest<Resp> {
     responder: oneshot::Sender<Resp>,
 }
 
-#[must_use = "You must respond to the request"]
-pub struct UnfulfilledRequest<Resp> {
-    responder: oneshot::Sender<Resp>,
-}
-
-impl<Req, Resp> IncomingRequest<Req, Resp>
+impl<Resp> UnRespondedRequest<Resp>
 where
     Resp: Debug,
 {
+    /// Fullfill our obligation to the [`Requester`].
     pub fn respond(self, response: Resp) -> Result<(), Resp> {
         self.responder.send(response)
     }
 }
 
-impl<Req, Resp> From<(Req, UnfulfilledRequest<Resp>)> for IncomingRequest<Req, Resp> {
-    fn from((request, unfulfilled_request): (Req, UnfulfilledRequest<Resp>)) -> Self {
-        Self {
-            request,
-            responder: unfulfilled_request.responder,
-        }
-    }
-}
-
+/// Represents the initiator for the request-response exchange
 pub struct Requester<Req, Resp> {
-    outgoing: channel::Sender<IncomingRequest<Req, Resp>>,
+    outgoing: channel::Sender<(Req, UnRespondedRequest<Resp>)>,
 }
 
 impl<Req, Resp> Requester<Req, Resp> {
-    pub async fn send(
-        &self,
-        request: Req,
-    ) -> Result<FutureResponse<Resp>, channel::SendError<IncomingRequest<Req, Resp>>> {
+    /// Make a request.
+    /// The [`FutureResponse`] should be `await`ed to get the response from the responder
+    pub async fn send(&self, request: Req) -> Result<FutureResponse<Resp>, Req> {
+        // Create the return path
         let (response_sender, response_receiver) = oneshot::channel();
         self.outgoing
-            .send(IncomingRequest {
+            .send((
                 request,
-                responder: response_sender,
-            })
-            .await?;
+                UnRespondedRequest {
+                    responder: response_sender,
+                },
+            ))
+            .await
+            .map_err(|e| e.into_inner().0)?;
         Ok(response_receiver)
     }
 }
 
 #[cfg_attr(doc, aquamarine::aquamarine)]
 /// Create a bounded [`Requester`]-[`Responder`] pair.  
-/// Once the channel is full, future senders will yield when awaiting
+/// That is, once the channel is full, future senders will yield when awaiting until there's space again
 ///
 /// Terminology is as follows:
 /// ```mermaid
@@ -74,8 +60,8 @@ impl<Req, Resp> Requester<Req, Resp> {
 ///     Responder ->> Requester: response
 /// ```
 ///
-/// When a [`Responder`] is asked to receive a request, it returns an [`IncomingRequest`].
-/// This may be dereferenced to the underlying request
+/// When a [`Responder`] is asked to receive a request, it returns a `(Request, [UnRespondedRequest<Response>])` pair.
+/// The latter should be used to communicate back to the sender
 ///
 /// ```
 /// # async_std::task::block_on( async {
@@ -83,16 +69,19 @@ impl<Req, Resp> Requester<Req, Resp> {
 /// let response = requester.send(String::from("hello")).await.unwrap();
 ///
 /// // This side of the channel receives strings, and responds with their length
-/// let request = responder.recv().await.unwrap();
+/// let (request, unfulfilled_response) = responder.recv().await.unwrap();
 /// let len = request.len();
-/// request.respond(len).unwrap();
+/// unfulfilled_response.respond(len).unwrap();
 ///
 /// assert!(response.await.unwrap() == 5);
 /// # })
 /// ```
 pub fn bounded<Req, Resp>(
     capacity: usize,
-) -> (Requester<Req, Resp>, Responder<IncomingRequest<Req, Resp>>) {
+) -> (
+    Requester<Req, Resp>,
+    Responder<(Req, UnRespondedRequest<Resp>)>,
+) {
     let (sender, receiver) = channel::bounded(capacity);
     (Requester { outgoing: sender }, receiver)
 }
